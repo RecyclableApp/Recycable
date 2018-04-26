481cc01efc800a5de2a87eaffc08d7feeb4b6f12
aliperez <alicia.perez@utexas.edu>
2018-04-26 14:33:43 -0700

    cleaned up student to be user

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-    var Student = sequelize.define("Student", {
+    var User = sequelize.define("User", {
         name: {
             type: DataTypes.STRING
         },
@@ -24,5 +24,5 @@ module.exports = function(sequelize, DataTypes) {
 
     });
 
-    return Student;
+    return User;
 };
